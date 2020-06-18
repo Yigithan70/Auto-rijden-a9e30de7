486a84a092a6757b0ad8f25b3a0a39a06305b462
Yigithan Dincer <2084054@talnet.nl>
2020-06-18 12:41:42 +0200

    Uitwerking van de exercise

--- rijbewijs.php
+++ rijbewijs.php
@@ -5,8 +5,7 @@ $line2 = readline("Hoe oud ben je? ");
 readline_add_history($line);
 if ($line2 >= 16.50) {
 	echo "Je mag beginnen met rijlessen!";
-} 
-else {
+} else {
 	echo "Helaas, je mag nog niet beginnen met rijlessen";
 }
 
